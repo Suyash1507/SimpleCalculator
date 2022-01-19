03fd7733bc12f64c4e27430a01b0ee2e26405076
SUYASH <64803486+Suyash1507@users.noreply.github.com>
2022-01-19 16:52:27 +0530

    Add files via upload

---
+++ Simple Calculator/MainPage.xaml.cs
@@ -0,0 +1,111 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace Simple_Calculator
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+        private decimal firstNumber;
+        private string operatorName;
+        private bool OperatorClicked;
+
+        private void BtnCommon_Clicked(object sender, EventArgs e)
+        {
+            var button= sender as Button;
+            if (LblResult.Text == "0" || OperatorClicked)
+            {
+                OperatorClicked = false;
+                LblResult.Text = button.Text;
+            }
+            else
+            {
+                LblResult.Text += button.Text;
+            }
+        }
+
+        private void BtnClear_Clicked(object sender, EventArgs e)
+        {
+            LblResult.Text = "0";
+        }
+
+        private void BtnX_Clicked(object sender, EventArgs e)
+        {
+            string number= LblResult.Text;
+            if (number != "0")
+            {
+                number = number.Remove(number.Length - 1);
+                if(string.IsNullOrEmpty(number))
+                {
+                    LblResult.Text= "0";
+                }
+                else
+                {
+                    LblResult.Text = number;
+                }
+            }
+        }
+
+        private async void BtnPercentage_Clicked(object sender, EventArgs e)
+        {
+            try
+            {
+                string number = LblResult.Text;
+                if(number != "0")
+                {
+                    decimal percentValue= Convert.ToDecimal(number); 
+                    string result= (percentValue/100).ToString("0.##");
+                    LblResult.Text = result;
+                }
+            }
+            catch(Exception ex)
+            {
+                await DisplayAlert("Error", ex.Message, "Ok");
+            }
+        }
+        private void BtnCommonOperation_Clicked(object sender, EventArgs e)
+        {
+            var button= sender as Button;
+            OperatorClicked = true;
+            operatorName = button.Text;
+            firstNumber = Convert.ToDecimal(LblResult.Text);
+
+        }
+
+        private void BtnEqual_Clicked(object sender, EventArgs e)
+        {
+            decimal secondNumber= Convert.ToDecimal(LblResult.Text);
+            string finalResult = Calculate(firstNumber, secondNumber).ToString("0.##");
+            LblResult.Text= finalResult;
+        }
+        public decimal Calculate(decimal firstNumber, decimal secondNumber)
+        {
+            decimal result=0;
+            if (operatorName == "+")
+            {
+                result= firstNumber+secondNumber;
+            }
+            else if (operatorName == "-")
+            {
+                result= firstNumber - secondNumber;
+            }
+            else if (operatorName == "*")
+            {
+                result = firstNumber * secondNumber;
+            }
+            else if (operatorName == "/")
+            {
+                result = firstNumber / secondNumber;
+            }
+            return result;
+        }
+    }
+}
